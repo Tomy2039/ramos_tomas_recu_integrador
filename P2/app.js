f23692e06fd32aa5eaf4949ee78d7980fea87986
Tomas <tramosyahari@gmal.com>
2024-07-04 15:15:02 -0300

    Esta hecho las rutas del listado y agregacion de estudiantes

--- P2/app.js
+++ P2/app.js
@@ -10,11 +10,6 @@ app.use(express.json());
 //configuracion
 app.set("port", process.env.PORT || 4321);
 
-//servidor
-app.listen(app.get("port"), () => {
-    console.log(`el servidor esta funcionando en el puerto ${app.get("port")}`);
-})
-
 //rutas 
 //Lista de estudiantes
 app.get("/students", (req, res) =>{
@@ -25,4 +20,23 @@ app.get("/students/:id", (req, res) =>{
     const id = +req.params.id;
     const estudianteId = estudiantes.findIndex(e => e.id === id);
     res.json(estudianteId);
+})
+
+//agregar nuevos estudiantes
+app.post("/students", (req, res) => {
+    const {fullName, age, curse} = req.body;
+    const id = new Date().getTime();
+    const nuevoEstudiante = {
+        id,
+        fullName,
+        age,
+        curse
+    }
+    estudiantes.push(nuevoEstudiante);
+    res.json(estudiantes);
+})
+
+//servidor
+app.listen(app.get("port"), () => {
+    console.log(`el servidor esta funcionando en el puerto ${app.get("port")}`);
 })
